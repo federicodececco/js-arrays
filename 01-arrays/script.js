9ca19dc7a2989e4f62ee60681372648bcce40718
federicodececco <federicodececco1@gmail.com>
2024-12-02 13:14:50 +0100

    6

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -23,9 +23,10 @@ const firstTeacher = teachers.shift();
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push("Vanessa")
-console.log(teachers)
-// 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
+// 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah")
+console.log(teachers)
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = null;
